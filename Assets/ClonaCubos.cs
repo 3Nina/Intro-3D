a0862eff7144db258e985963c88b63b5b1c59493
3Nina <167197662+3Nina@users.noreply.github.com>
2025-05-16 10:35:59 -0300

    Clona cubos completo

---
+++ Assets/ClonaCubos.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClonaCubos : MonoBehaviour
+{
+    public GameObject cuboPrefab;
+    void Start()
+    {
+        
+    }
+
+    public void ClonarCubos()  // Sin una funión publica el botón no puede ser ejecutado
+    {
+        Instantiate(cuboPrefab);
+    }
+
+
+}
